a7cc2c5d7b15fdb86f697da452d168064eed8a52
chau nguyen <nhbc111@yahoo.com>
2017-03-09 22:20:22 -0500

    added assosiation

--- app/models/invest.rb
+++ app/models/invest.rb
@@ -1,2 +1,5 @@
 class Invest < ApplicationRecord
+
+  belong_to :user
+  belong_to :etf
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :invest dependent: :delete_all
+  belong_to :question
 end
