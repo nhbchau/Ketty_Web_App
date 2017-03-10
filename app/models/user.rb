ef8a6ce7d95865586374d73f9aaae6dcbbb9b425
chau nguyen <nhbc111@yahoo.com>
2017-03-10 15:35:32 -0500

    added validates for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  validates :email, presence: true
+  validates :name, presence: true
+  validates :question_id, presence: true
+
   has_many :invest dependent: :delete_all
   belong_to :question
 end
