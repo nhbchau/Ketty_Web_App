ca83aa79f6e897c0aecb5161338c3c0066ae39da
chau nguyen <nhbc111@yahoo.com>
2017-03-22 19:12:38 -0400

    added validation for etf

--- app/models/etf.rb
+++ app/models/etf.rb
@@ -1,5 +1,16 @@
 class Etf < ApplicationRecord
+  validates :etf_name, uniqueness: true, presence: true
+  validates :branch, presence: true
+  validates :type_etf, presence: true
+  validates :symbol, presence: true
+  validates :experience, presence: true, numericality: {only_float: true}
+  validates :fees, presence: true, numericality: {only_float: true}
+  validates :liquidity, presence:true, numericality: {only_float: true}
+  validates :size, presence:true, numericality: {only_float: true}
+  validates :perfomance, presence:true, numericality: {only_float: true}
+  validates :yeild, presence:true numericality: {only_float: true}
+  validates :price, presence:true numericality: {only_float: true} 
 
   has_many :invests
-  
+
 end
