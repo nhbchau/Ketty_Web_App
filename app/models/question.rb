65ca34c1664bb9e3a7a0201175fe06e91b458099
chau nguyen <nhbc111@yahoo.com>
2017-03-22 21:20:50 -0400

    added validation for question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,27 @@
 class Question < ApplicationRecord
+  validates :age, presence:true
+  validates :married, presence:true
+  validates :kids, presence:true
+  validates :income_you_earn, presence:true
+  validates :money_invest, presence:true
+  validates :income_from_investment, presence:true
+  validates :need_these_funds, presence:true
+  validates :stomach_a_loss, presence:true
+  validates :risk_taker, presence:true
+  validates :cash, presence:true, numericality:{only_float: true}
+  validates :cash_current, presence: true, numericality: {only_float: true}
+  validates :bonds, presence:true, numericality: {only_float}
+  validates :bonds_short_term, presence:true, numericality: {only_float: true}
+  validates :bonds_mid_term, presence:true, numericality: {only_float: true}
+  validates :bonds_long_term, presence:true, numericality: {only_float: true}
+  validates :stocks, presence:true, numericality: {only_float:true}
+  validates :stocks_canadian, presence:true, numericality: {only_float:true}
+  validates :stocks_us, presence:true, numericality: {only_float:true}
+  validates :stocks_international, presence:true, numericality: {only_float:true}
+  validates :risk_score, presence:true, numericality: {only_float:true}
+  validates :target_return, presence:true, numericality: {only_float:true}
+
+
   has_one :user
 
 end
