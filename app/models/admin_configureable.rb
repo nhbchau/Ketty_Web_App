868a8ea32b97786443c78ba2d2a7bfbf42bc9029
chau nguyen <nhbc111@yahoo.com>
2017-03-22 21:28:07 -0400

    added validation for admin configureable

--- app/models/admin_configureable.rb
+++ app/models/admin_configureable.rb
@@ -1,2 +1,17 @@
 class AdminConfigureable < ApplicationRecord
+
+  validates :bonds_short_term, presence:true, numericality: {only_float: true}
+  validates :bonds_mid_term, presence:true, numericality: {only_float: true}
+  validates :bonds_long_term, presence:true, numericality: {only_float: true}
+  validates :stocks_canadian, presence:true, numericality: {only_float: true}
+  validates :stocks_us, presence:true, numericality: {only_float: true}
+  validates :stocks_international, presence:true, numericality: {only_float: true}
+  validates :monitoring_percent, presence:true, numericality: {only_integer: true}
+  validates :very_low_setup, presence:true, numericality: {only_float: true}
+  validates :very_low_annual, presence:true, numericality: {only_float: true}
+  validates :low_setup, presence:true, numericality: {only_float: true}
+  validates :low_annual, presence:true, numericality: {only_float: true}
+  validates :controllable_setup, presence:true, numericality: {only_float: true}
+  validates :controllable_annual, presence:true, numericality: {only_float: true}
+
 end
